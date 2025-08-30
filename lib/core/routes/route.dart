070912aa3a8b3eba49c4e919d613e0e3f201fcbc
mohamed <eng.mohamed.fayad33@gmail.com>
2025-08-30 14:46:14 +0400

    initialize routs

--- lib/core/routes/route.dart
+++ lib/core/routes/route.dart
@@ -1,1 +1,11 @@
-abstract class Route {}
+import 'package:flutter/material.dart';
+import 'package:movies_app/core/routes/pages_routes_name.dart';
+import 'package:movies_app/modules/splash/splash_view.dart';
+
+abstract class Routes {
+  static Map<String, Widget Function(BuildContext)> route(
+    BuildContext context,
+  ) {
+    return {PagesRoutesName.splash: (context) => const SplashView()};
+  }
+}
